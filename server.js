bf18c837c2daee12b363f0e7ccb139255791638d
RohanMaganahalli <rohan7@techie.com>
2024-06-02 18:08:42 +0530

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  const yourName = "Rohan";
+
+  res.end(yourName);
+  
+  console.log(yourName);
+});
+
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
