8c8e38d457d1b16d0e5f0a7e4ad6d9987f669485
RohanMaganahalli <rohan7@techie.com>
2024-06-02 18:09:53 +0530

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   
-  const yourName = "Rohan";
+  const yourName = "Rohan M";
 
   res.end(yourName);
   
