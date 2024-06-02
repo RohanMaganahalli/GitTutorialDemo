956fee39d2b6e52ed25d60d972a5fc46bb7ba945
RohanMaganahalli <rohan7@techie.com>
2024-06-02 18:27:53 +0530

    about

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
-  const yourName = "Rohan";
+  // Parse the request URL
+  const parsedUrl = url.parse(req.url, true);
 
-  res.end(yourName);
-  
-  console.log(yourName);
+  // Get the path from the URL
+  const path = parsedUrl.pathname;
+
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Handle different paths
+  if (path === '/home') {
+    res.end('Welcome home');
+  } else if (path === '/about') {
+    res.end('Welcome to About Us page');
+  } else if (path === '/node') {
+    res.end('Welcome to my Node Js project');
+  } else {
+    // If the path doesn't match any of the above, return a default response
+    res.end('Welcome to my Node Js project');
+  }
 });
 
 server.listen(4000, () => {
